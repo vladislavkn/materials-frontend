d112e0125cda7689925991467bd75674e730e979
Vladislav Knyshov <inova136@gmail.com>
2022-07-10 14:37:51 +0300

    Connect tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
-const Home: React.FC = () => <div>Hello world!</div>
+const Home: React.FC = () => (
+  <>
+    <header>
+      <h1 className="font-sans">Materials</h1>
+    </header>
+    <main></main>
+  </>
+)
 
 export default Home
