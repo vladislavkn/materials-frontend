899e19e7850b25553d339c2a03a2de861551b644
Vladislav Knyshov <inova136@gmail.com>
2022-07-15 20:05:27 +0300

    add Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer: React.FC = () => (
+  <footer className="border-t border-gray-400 mt-12">
+    <div className="container py-6">
+      <p>
+        Made by <a href="#">@vladislavkn</a>
+      </p>
+      <p>
+        See this project on <a href="#">Github</a>
+      </p>
+    </div>
+  </footer>
+)
+
+export default Footer
