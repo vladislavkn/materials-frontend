2ec72e8f83ae98e71e30b6c60c657080b0ad0533
Vladislav Knyshov <inova136@gmail.com>
2022-07-16 12:26:28 +0300

    wrap app with the redux provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import store from '../store'
 import '../styles/globals.css'
 
 const CustomApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>
 )
 
 export default CustomApp
