6168159711d484e53a18f7b95df4afcb12200d4f
Vladislav Knyshov <inova136@gmail.com>
2022-07-16 12:26:01 +0300

    fix footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer: React.FC = () => (
-  <footer className="border-t border-gray-400 mt-12">
+  <footer className="border-t border-gray-400 mt-12 text-gray-800">
     <div className="container py-6">
       <p>
         Made by <a href="#">@vladislavkn</a>
