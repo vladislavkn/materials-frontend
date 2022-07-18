1ff2d631c44e0440b8ee24aa613fc19613b4ab76
Vladislav Knyshov <inova136@gmail.com>
2022-07-18 14:34:27 +0300

    update articles component

--- components/Articles.tsx
+++ components/Articles.tsx
@@ -1,6 +1,9 @@
+import { SerializedError } from '@reduxjs/toolkit'
+import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query'
 import { FC } from 'react'
 import { useGetArticlesQuery } from '../store/articles.api'
 import Article from './Article'
+import ErrorFallback from './ErrorFallback'
 
 type ArticlesProps = {
   searchValue: string
@@ -12,13 +15,14 @@ const Articles: FC<ArticlesProps> = ({ searchValue }) => {
   })
 
   if (isLoading) return <span>Loading...</span>
-  if (error) return <span>{error.toString()}</span>
+  if (error) return <ErrorFallback error={error} />
 
   return (
     <div className="grid gap-8 grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {data.map((article) => (
         <Article article={article} key={article.id} />
       ))}
+      {data.length === 0 && 'No articles found'}
     </div>
   )
 }
