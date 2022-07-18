3faccec0dc6c8ae95d60a48f884bd81bfa5b96b6
Vladislav Knyshov <inova136@gmail.com>
2022-07-18 14:21:54 +0300

    add login 7 registration pages

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,13 +1,26 @@
-import { FC } from 'react'
+import { useRouter } from 'next/router'
+import { FC, useEffect } from 'react'
 import Layout from '../components/Layout'
 import LoginForm from '../components/LoginForm'
+import { useAppDispatch, useAppSelector } from '../store'
+import { login, selectUser } from '../store/auth.slice'
 
 const LoginPage: FC = () => {
+  const router = useRouter()
+  const user = useAppSelector(selectUser)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    if (user) router.push('/profile')
+  }, [user])
+
+  const handleSubmit = (email, password) => {
+    dispatch(login({ email, password }))
+  }
+
   return (
     <Layout>
-      <LoginForm
-        onSubmit={(email, password) => console.table({ email, password })}
-      />
+      <LoginForm onSubmit={handleSubmit} />
     </Layout>
   )
 }
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,15 +1,26 @@
-import { FC } from 'react'
+import { useRouter } from 'next/router'
+import { FC, useEffect } from 'react'
 import Layout from '../components/Layout'
 import RegistrationForm from '../components/RegistrationForm'
+import { useAppDispatch, useAppSelector } from '../store'
+import { register, selectUser } from '../store/auth.slice'
 
 const RegistrationPage: FC = () => {
+  const router = useRouter()
+  const user = useAppSelector(selectUser)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    if (user) router.push('/login')
+  }, [user])
+
+  const handleSubmit = (email, name, password) => {
+    dispatch(register({ email, name, password }))
+  }
+
   return (
     <Layout>
-      <RegistrationForm
-        onSubmit={(email, name, password) =>
-          console.table({ email, name, password })
-        }
-      />
+      <RegistrationForm onSubmit={handleSubmit} />
     </Layout>
   )
 }
