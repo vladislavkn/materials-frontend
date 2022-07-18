bfa5e42f0c955628f8ae66bd52ee8763374e62be
Vladislav Knyshov <inova136@gmail.com>
2022-07-18 14:22:04 +0300

    add profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,15 +1,32 @@
-import { useRouter } from 'next/router'
-import { FC, useEffect } from 'react'
+import { FC } from 'react'
 import Layout from '../components/Layout'
+import useUser from '../hooks/useUser'
+import { useAppDispatch } from '../store'
+import { logout } from '../store/auth.slice'
 
 const ProfilePage: FC = () => {
-  const router = useRouter()
+  const user = useUser('/login')
+  const dispatch = useAppDispatch()
 
-  useEffect(() => {
-    router.push('/login')
-  }, [])
+  const handleLogout = () => {
+    dispatch(logout())
+  }
 
-  return <Layout />
+  if (!user) return <Layout>Loading...</Layout>
+
+  return (
+    <Layout>
+      <header className="flex items-center justify-between text-gray-800">
+        <div className="flex gap-2 flex-col">
+          <h2 className="font-medium text-4xl">{user.name}</h2>
+          <p className="text-gray-400">{user.role.toLowerCase()}</p>
+        </div>
+        <button onClick={handleLogout} className="underline">
+          Logout
+        </button>
+      </header>
+    </Layout>
+  )
 }
 
 export default ProfilePage
